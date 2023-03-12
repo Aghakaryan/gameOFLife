0bbd34a8fd60b079170cf34194c84c4e9eee32e0
Aghakaryan <aram.aghakaryan@tumo.org>
2023-03-12 12:52:03 +0400

    jarangakanutyun

--- programming2/GrassBuilder.js
+++ programming2/GrassBuilder.js
@@ -1,7 +1,6 @@
-class GrassBuilder {
+class GrassBuilder extends LivingCreature {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
+        super(x,y)
         this.directions = [
             [this.x, this.y - 1],
             [this.x - 1, this.y],
@@ -16,23 +15,6 @@ class GrassBuilder {
             [this.x + 1, this.y],
             [this.x, this.y + 1],
         ];
-    }
-    chooseCell(char) {
-        this.getNewCordinates();
-        let result = [];
-
-        for (let i = 0; i < this.directions.length; i++) {
-            let x = this.directions[i][0];
-            let y = this.directions[i][1];
-
-            if (y < matrix.length && y >= 0 && x < matrix[0].length && x >= 0) {
-                if (matrix[y][x] == char) {
-                    result.push(this.directions[i]);
-                }
-            }
-
-        }
-        return result;
     }
     eat() {
         let found = this.chooseCell(0);
